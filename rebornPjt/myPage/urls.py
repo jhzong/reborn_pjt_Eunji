cbfd751294ed3333e65cf2f436a0c628aa4a8e44
oeunji9127-maker <oeunji9127@gmail.com>
2026-01-13 12:15:04 +0900

    0113 스토어 오은지

--- rebornPjt/myPage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='mypage'
-urlpatterns = [
-    path('My_main/', views.My_main, name='My_main'),
-    
-]
--- rebornPjt/myPage/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def My_main(request):
-    return render(request,'myPage/My_main.html')
